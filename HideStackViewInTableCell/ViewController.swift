586c9cb8bf12bda9d69c350f69bafbf7cc4363b8
Jefferson Setiawan <jefferson.setiawan@tokopedia.com>
2018-06-26 12:22:26 +0700

    Initial Commit

---
+++ HideStackViewInTableCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HideStackViewInTableCell
+//
+//  Created by Jefferson Setiawan on 26/06/18.
+//  Copyright © 2018 Jefferson Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
